2eceee2b6f83f7f3aed80a2885bdf96e29a46de1
DoanCongQui <congt4644@gmail.com>
2024-03-23 11:25:17 +0700

    Test 23 03

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
 from Motor import Motor
 
-motor = Motor(17, 27, 22)
+motor = Motor(17, 27, 22, 23, 24, 25)
 
 motor.chay(speed=0.5, t=10)
 
         # Chay max 
 #  motor.chay(speed=1, t=20)
 
-#  motor.trai(speed=0.5, t=10)
+motor.trai(speed=0.5, t=3)
 #  motor.phai(speed=0.5, t=10)
 
 #  motor.trai_tt(speed=0.5, t=10)
--- Motor.py
+++ Motor.py
@@ -5,32 +5,42 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 class Motor():
-    # def __init__(self, EnaA, In1A, In2A, EnaB, In1B, In2B):
-    def __init__(self, EnaA, In1A, In2A):
+    def __init__(self, EnaA, In1A, In2A, EnaB, In1B, In2B):
+    # def __init__(self, EnaA, In1A, In2A):
         # Dong co banh sau 
         self.EnaA = EnaA
         self.In1A = In1A
         self.In2A = In2A
 
         # Dong co banh truoc 
-        # self.EnaB = EnaB
-        # self.In1B = In1B
-        # self.In2B = In2B
+        self.EnaB = EnaB
+        self.In1B = In1B
+        self.In2B = In2B
 
         GPIO.setup(self.EnaA, GPIO.OUT)
         GPIO.setup(self.In1A, GPIO.OUT)
         GPIO.setup(self.In2A, GPIO.OUT)
-        # GPIO.setup(self.EnaB, GPIO.OUT)
-        # GPIO.setup(self.In1B, GPIO.OUT)
-        # GPIO.setup(self.In2B, GPIO.OUT)
+        GPIO.setup(self.EnaB, GPIO.OUT)
+        GPIO.setup(self.In1B, GPIO.OUT)
+        GPIO.setup(self.In2B, GPIO.OUT)
 
         self.pwmA = GPIO.PWM(self.EnaA, 100)
-        # self.pwmB = GPIO.PWM(self.EnaB, 100)
+        self.pwmB = GPIO.PWM(self.EnaB, 100)
         self.pwmA.start(0)
-        # self.pwmB.start(0)
+        self.pwmB.start(0)
 
-    # Ham chay
+    # Ham chay toi
     def chay(self, speed=0.5, t=0):
+        GPIO.output(self.In1A, GPIO.LOW)
+        GPIO.output(self.In2A, GPIO.HIGH)
+        self.pwmA.ChangeDutyCycle(speed * 100)
+
+        # Dung dong co
+        sleep(t)
+        self.pwmA.ChangeDutyCycle(0)
+
+    # Ham chay lui
+    def lui(self, speed=0.5, t=0):
         GPIO.output(self.In1A, GPIO.HIGH)
         GPIO.output(self.In2A, GPIO.LOW)
         self.pwmA.ChangeDutyCycle(speed * 100)
@@ -39,42 +49,41 @@ class Motor():
         sleep(t)
         self.pwmA.ChangeDutyCycle(0)
 
+    # Ham re trai
+    def trai(self, speed=0.5):
+        GPIO.output(self.In1B, GPIO.HIGH)
+        GPIO.output(self.In2B, GPIO.LOW)
+        self.pwmB.ChangeDutyCycle(speed * 100)
 
-    # # Ham re trai
-    # def trai(self, speed=0.5):
-    #     GPIO.output(self.In1B, GPIO.HIGH)
-    #     GPIO.output(self.In2B, GPIO.LOW)
-    #     self.pwmB.ChangeDutyCycle(speed * 100)
-
-    # # Ham re phai
-    # def phai(self, speed=0.5):
-    #     GPIO.output(self.In1B, GPIO.LOW)
-    #     GPIO.output(self.In2B, GPIO.HIGH)
-    #     self.pwmB.ChangeDutyCycle(speed * 100)
-
+    # Ham re phai
+    def phai(self, speed=0.5):
+        GPIO.output(self.In1B, GPIO.LOW)
+        GPIO.output(self.In2B, GPIO.HIGH)
+        self.pwmB.ChangeDutyCycle(speed * 100)
 
 
-    # # Demo mo cu dong cua 
-    # def trai_tt(self, speed=0.5, t=2):
-    #     GPIO.output(self.In1B, GPIO.HIGH)
-    #     GPIO.output(self.In2B, GPIO.LOW)
+
+    # Demo mo cu dong cua 
+    def trai_tt(self, speed=0.5, t=2):
+        GPIO.output(self.In1B, GPIO.HIGH)
+        GPIO.output(self.In2B, GPIO.LOW)
         
-    #     for i in range(0, int(speed*100), 5):
-    #         self.pwmB.ChangeDutyCycle(i)
-    #         sleep(t / (speed*100))
+        for i in range(0, int(speed*100), 5):
+            self.pwmB.ChangeDutyCycle(i)
+            sleep(t / (speed*100))
 
-    #     # Dung dong co
-    #     sleep(t)
-    #     self.pwmB.ChangeDutyCycle(0)
+        # Dung dong co
+        sleep(t)
+        self.pwmB.ChangeDutyCycle(0)
 
-    # def phai_tt(self, speed=0.5, t=2):
-    #     GPIO.output(self.In1B, GPIO.LOW)
-    #     GPIO.output(self.In2B, GPIO.HIGH)
+    def phai_tt(self, speed=0.5, t=2):
+        GPIO.output(self.In1B, GPIO.LOW)
+        GPIO.output(self.In2B, GPIO.HIGH)
         
-    #     for i in range(0, int(speed*100), 5):
-    #         self.pwmB.ChangeDutyCycle(i)
-    #         sleep(t / (speed*100))
+        for i in range(0, int(speed*100), 5):
+            self.pwmB.ChangeDutyCycle(i)
+            sleep(t / (speed*100))
 
-        # # Dung dong co
-        # sleep(t)
-        # self.pwmB.ChangeDutyCycle(0)
+        # Dung dong co
+        sleep(t)
+        self.pwmB.ChangeDutyCycle(0)

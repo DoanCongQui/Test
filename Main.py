ac9f7d1a5e66b86a647f042edb9289984c2350a7
DoanCongQui <congt4644@gmail.com>
2024-03-23 11:39:03 +0700

    Test 23 03

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 from Motor import Motor
+import RPi.GPIO as GPIO
+from time import sleep
 
 motor = Motor(17, 27, 22, 23, 24, 25)
 
@@ -13,3 +15,17 @@ motor.trai(speed=0.5, t=3)
 #  motor.trai_tt(speed=0.5, t=10)
 #  motor.phai_tt(speed=0.5, t=10)
 
+
+try:
+    # Rẽ trái trong 2 giây, sau đó trở lại vị trí ban đầu
+    motor.trai_tt(speed=0.5, t=2)
+    print("Turned left for 2 seconds.")
+
+    sleep(2)  # Chờ 2 giây
+
+    # Rẽ phải trong 2 giây, sau đó trở lại vị trí ban đầu
+    motor.phai_tt(speed=0.5, t=2)
+    print("Turned right for 2 seconds.")
+
+finally:
+    GPIO.cleanup()  # Dọn dẹp GPIO khi kết thúc chương trình
